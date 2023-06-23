841b2ebaa9ccf0ee842711f7103bbccc699f38e9
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2023-06-23 14:20:22 +0200

    Init project

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Zad1 from "./views/Zad1";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from "react-bootstrap";
+function App() {
+  return (
+    <>
+      <Container fluid>
+        <Zad1/>
+      </Container>
+    </>
+  )
+}
+
+export default App
