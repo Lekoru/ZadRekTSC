5b718c041c035e379bc7d246b62953aaf22b2917
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2023-06-27 12:11:30 +0200

    Created simple Zad2 page to verify router

---
+++ Frontend/src/views/Zad2.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Grid} from "@mui/material";
+
+export const Zad2 = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid item>
+          Zadanie 2
+        </Grid>
+      </Grid>
+    </>
+  )
+}
