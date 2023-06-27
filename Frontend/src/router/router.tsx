e8ce0cc7f32e514d1b0b514133737f99894f969e
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2023-06-27 12:11:39 +0200

    Added new route to router.tsx

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 import React from "react";
 import Zad1 from "../views/Zad1";
+import {Zad2} from "../views/Zad2";
 export const Router = () => {
   return(
     <Routes>
       <Route path='/' element={<Zad1/>}/>
+      <Route path='/Zad2' element={<Zad2/>}/>
       <Route path='*' element={<Zad1/>}/>
     </Routes>
   )
