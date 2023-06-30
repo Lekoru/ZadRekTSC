0a117933d035b6c4d306f4a0f0c0c9b335f92380
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2023-06-30 15:46:51 +0200

    Fixed Eslint "interface" reserved error for TypeScript

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -1,15 +1,24 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json'},
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint'],
+  root: true,
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react/react-in-jsx-scope": "off",
+    "camelcase": "warn",
+    "quotes": ["error", "single"],
+    "no-extra-semi": "warn",
+    "semi": "warn",
+    "no-multi-spaces": "warn",
+    "space-before-function-paren": "warn",
+    "space-in-parens": "warn",
+    "object-curly-spacing": "warn",
+    "array-bracket-spacing":  "warn",
+    "no-unused-expressions": "warn",
+    "no-unused-labels": "warn",
+    "no-unused-vars": "warn"
   },
 }
